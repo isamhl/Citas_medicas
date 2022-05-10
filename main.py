19c312c35371400d151c615b6a126c993b9c929f
isamhl <isachirivel@gmail.com>
2022-05-10 22:47:50 +0200

    subida a git

---
+++ main.py
@@ -0,0 +1,80 @@
+from threading import local
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_mysqldb import MySQL
+import MySQLdb
+
+app = Flask(__name__)
+app.secret_key = "1234534534"
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "clinica"
+
+db = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+        if 'dni' in request.form and 'password' in request.form:
+            dni = request.form['dni']
+            password = request.form['password']
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM paciente WHERE dni=UPPER(%s) AND password=%s",(dni,password))
+            info = cursor.fetchone()
+            if info is not None:
+                if info['dni'] == dni and info["password"] == password:
+                    session['usuario'] = dni
+                    return redirect(url_for('citas'))
+            else:
+                return "fallo con el acceso, porfavor registrese primero"
+        else:
+            return "falta información para el acceso"
+    else:
+        return render_template('index.html')
+
+
+@app.route('/new')
+def register():
+    return render_template('register.html')
+
+@app.route('/citas')
+def citas():
+    if session['usuario'] is not None:
+        dni = session['usuario']
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM cita JOIN paciente ON cita.paciente = paciente.id  WHERE paciente.dni=%s",(dni,))
+        info = cursor.fetchall()
+        return render_template('citas.html', data=info)
+    else:
+        return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('index'))
+
+@app.route('/new',methods=['POST'])
+def new():
+    dni = request.form['dni']
+    password = request.form['password']
+    nombre = request.form['nombre']
+    apellidos = request.form['apellidos']
+    localidad = request.form['localidad']
+    
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM paciente WHERE dni='{{dni}}'")
+            
+    info = cursor.fetchone()
+    if info is None:
+        consulta = "insert into paciente (dni, nombre, apellidos, localidad, password) values (UPPER(%s), %s, %s, %s, %s)"
+        cursor.execute(consulta,(dni,nombre,apellidos,localidad,password))
+        db.connection.commit()
+        return render_template('index.html')
+    else:
+        return "ya se encuentra registrado un usuario con dni: "+dni
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
